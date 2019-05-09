daf24bb8978d1d47b8b5ebc0e0e9920b8abb59dc
Amanda <amandamojabi@gmail.com>
2019-05-08 19:14:23 -0700

    Checkpoint 5 update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function onReady()  {
+function onReady() {
   const addToDoForm= document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit',() => {
+  addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
 
     // get the text
@@ -28,9 +28,10 @@ function onReady()  {
     toDoList.appendChild(newLi);
 
     // empty the input
-    newToDoText = '';
+    newToDoText.value = '';
+
   });
 }
 window.onload = function() {
-  onReady
+  onReady();
 };
