d5c022852a212041ddeb2a4becf47a21fafe2b0c
Amanda <amandamojabi@gmail.com>
2019-05-09 21:22:37 -0700

    Assignment 5 completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function onReady()  {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit',() => {
+  addToDoForm.addEventListener('submit',event => {
     event.preventDefault();
 
     // get the text
@@ -18,19 +18,29 @@ function onReady()  {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event){
+      //this.parentElement represents the button's <li> parent
+      toDoList.removeChild(this.parentElement);
+    })
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    newLi.appendChild(deleteBtn);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
-    newToDoText = '';
+    newToDoText.value = "";
   });
 }
 window.onload = function() {
-  onReady
+  onReady();
 };
