a380c8efeaef1e7e424b1f90f04182897e9f0184
Amanda <amandamojabi@gmail.com>
2019-05-10 17:25:46 -0700

    assignment 6 FEintensive check

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
 function onReady() {
-  const toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm');
-}
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  let toDos = [];
+  let id = 0;
 
-function createNewToDo() {
-  const newToDoText = document.getElementById('newToDoText');
-  if (!newToDoText.value) {return; }
+  function renderTheUI(){
+    const TODO_LIST = document.getElementById('toDoList');
+    TODO_LIST.textContent = '';
 
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
+     toDos.forEach(function(toDo){
+       const NEW_LI = document.createElement('li');
+       const CHECKBOX  = document.createElement('input');
+       CHECKBOX.type = "checkbox";
+
+       const DELETE_BTN = document.createElement('button');
+       DELETE_BTN.textContent = "Delete!";
+
+       DELETE_BTN.addEventListener('click', event => {
+         alert(toDo.title);
+       //renderTheUI();
+      });
+
+       NEW_LI.textContent = toDo.title;
+
+       TODO_LIST.appendChild(NEW_LI);
+       NEW_LI.appendChild(CHECKBOX);
+       NEW_LI.appendChild(DELETE_BTN);
+    })
+
+  }
+
+  function createNewToDo(){
+    const NEW_TODO_TEXT = document.getElementById('newToDoText');
+    if(!NEW_TODO_TEXT.value){ return; }
+    toDos.push({
+      title: NEW_TODO_TEXT.value,
+      complete: false
+      id: id
+    });
+
+    id++;
+
+    NEW_TODO_TEXT.value = '';
+    renderTheUI();
+  }
+
+  ADD_TODO_FORM.addEventListener('submit', event => )
+    event.preventDefault();
+    createNewToDo();
   });
-  newToDoText.value = '';
 
   renderTheUI();
 }
-
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
-});
-
-function renderTheUI(); {
-  const toDoList = document.getElementById('toDoList');
-
-  toDoList.textContent = '';
-
-  toDos.forEach(function(toDo) {
-    const newLi = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-
-    newLi.textContent = toDo.title;
-
-    toDoList.appendChild(newLi);
-    newLi.appendChild(checkbox);
-  });
-
-  }
 window.onload = function() {
   onReady();
 };
